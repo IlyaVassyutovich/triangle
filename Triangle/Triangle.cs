a491cfcda51cef045231d6c83469701282123c69
Ilya Vassyutovich <MONOmah_V@mail.ru>
2015-08-10 18:37:19 +0300

    Init

---
+++ Triangle/Triangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleNS
+{
+    public class Triangle
+    {
+        public double a { get; protected set; }
+        public double b { get; protected set; }
+        public double c { get; protected set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            if ((a <= 0) || (b <= 0) || (c <= 0))
+            {
+                throw new ArgumentOutOfRangeException("Wrong side length");
+            }
+            if (!((a + b >= c) && (a + c >= b) && (b + c >= a)))
+            {
+                throw new ArgumentOutOfRangeException("Triangle inequality");
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double Area()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+
+    public class RightTriangle : Triangle
+    {
+        public RightTriangle(double a, double b, double c) : base(a, b, c)
+        {
+            double[] sides = new double[3] {a, b, c};
+            Array.Sort(sides);
+            if (Math.Abs(sides[0] * sides[0] + sides[1] * sides[1] - sides[2] * sides[2]) > 1E-5)
+            {
+                throw new ArgumentOutOfRangeException("Not right triangle");
+            }
+        }
+    }
+}
---
+++ TriangleTest/TriangleTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleNS;
+
+namespace TriangleTestNS
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleInequalityTest()
+        {
+            Triangle t = new Triangle(2, 3, 6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void WrongSideLegthTest()
+        {
+            Triangle t = new Triangle(1, 2, 0);
+        }
+
+        [TestMethod]
+        public void AreaTest()
+        {
+            Triangle t = new Triangle(3, 4, 5);
+
+            double expected = 3 * 4 / 2;
+            double area = t.Area();
+
+            Assert.AreEqual(expected, area, 1E-3);
+        }
+    }
+
+    [TestClass]
+    public class RightTriangleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NotRightTriangleTest()
+        {
+            RightTriangle t = new RightTriangle(1, 2, 3);
+        }
+
+        [TestMethod]
+        public void CorrectRightTriangleTest()
+        {
+            RightTriangle t = new RightTriangle(3, 4, 5);
+        }
+    }
+}
